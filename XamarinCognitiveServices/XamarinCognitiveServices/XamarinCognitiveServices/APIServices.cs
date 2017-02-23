730763c5bf2020aa84fe9bfca82142fdf477234e
Sergio Andrés Vargas @serandvaraco <svargas@appfuture.net>
2017-02-23 06:36:48 -0500

    Comentarios sobre la API

--- XamarinCognitiveServices/XamarinCognitiveServices/XamarinCognitiveServices/APIServices.cs
+++ XamarinCognitiveServices/XamarinCognitiveServices/XamarinCognitiveServices/APIServices.cs
@@ -3,19 +3,28 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-
-
-using Microsoft.ProjectOxford.Emotion;
 using System.Linq;
+
+#region Nuget's Requeridos
+using Microsoft.ProjectOxford.Emotion;
 using Microsoft.ProjectOxford.Vision.Contract;
 using Microsoft.ProjectOxford.Vision;
 using Plugin.Media.Abstractions;
 using Plugin.Media;
+#endregion 
 
 namespace XamarinCognitiveServices
 {
+    /// <summary>
+    /// Permite realizar la comunicación con las api's de servicios Cognitivos
+    /// </summary>
     public class APIServices
     {
+        /// <summary>
+        /// Permite analizar las emociones 
+        /// </summary>
+        /// <param name="stream">Recibe Imagen o Video </param>
+        /// <returns>Emociones obtenidas </returns>
         public static async Task<Dictionary<string, float>>
             GetEmotions(System.IO.Stream stream)
         {
@@ -30,25 +39,33 @@ namespace XamarinCognitiveServices
                             .ToDictionary(x => x.Key, x => x.Value);
         }
 
+        /// <summary>
+        /// Obtiene reconocimiento de imagenes usando CognitiveServices
+        /// </summary>
+        /// <param name="stream">Recibe Foto o Video</param>
+        /// <returns>Obtiene resultado del análisis </returns>
         public static async Task<AnalysisResult>
             GetAnalisysComputerVisio(System.IO.Stream stream)
         {
             VisionServiceClient client =
                 new VisionServiceClient("a051bcb1c4864f6188409e99010e177b");
-            VisualFeature[] features = {
-                                            VisualFeature.Adult,
-                                             VisualFeature.Faces,
-                                              VisualFeature.Color,
-                                               VisualFeature.Description,
-                                                VisualFeature.Tags,
-                                                 VisualFeature.ImageType,
-                                            VisualFeature.Categories };
+            VisualFeature[] features =
+                    {
+                      VisualFeature.Adult,
+                      VisualFeature.Faces,
+                      VisualFeature.Color,
+                      VisualFeature.Description,
+                      VisualFeature.Tags,
+                      VisualFeature.ImageType,
+                      VisualFeature.Categories
+            };
 
-            return await client.AnalyzeImageAsync(stream, features); 
+            return await client.AnalyzeImageAsync(stream, features);
         }
 
         //xam.Plugin.Media
-        public static async Task<MediaFile> TakePhoto(bool useCamera) {
+        public static async Task<MediaFile> TakePhoto(bool useCamera)
+        {
             await Plugin.Media.CrossMedia.Current.Initialize();
             if (useCamera)
                 if (!CrossMedia.Current.IsCameraAvailable ||
@@ -65,7 +82,7 @@ namespace XamarinCognitiveServices
                     })
                     : await CrossMedia.Current.PickPhotoAsync();
 
-            return file; 
+            return file;
         }
 
     }
