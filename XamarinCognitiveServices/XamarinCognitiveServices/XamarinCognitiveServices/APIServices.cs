af31ad0b6a33cce18dc145ca40edff9fcb49db56
= <svargas@appfuture.net>
2017-02-23 06:23:12 -0500

    Add Project XamarinCognitiveServices

---
+++ XamarinCognitiveServices/XamarinCognitiveServices/XamarinCognitiveServices/APIServices.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Microsoft.ProjectOxford.Emotion;
+using System.Linq;
+using Microsoft.ProjectOxford.Vision.Contract;
+using Microsoft.ProjectOxford.Vision;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+
+namespace XamarinCognitiveServices
+{
+    public class APIServices
+    {
+        public static async Task<Dictionary<string, float>>
+            GetEmotions(System.IO.Stream stream)
+        {
+
+            EmotionServiceClient clientEmotion = new EmotionServiceClient("cc6f615ca9ad42e0b4df4f78718adbbd");
+            var emotion = await clientEmotion.RecognizeAsync(stream);
+
+            if (emotion == null || emotion.Count() == 0)
+                return null;
+            return emotion[0].Scores
+                            .ToRankedList()
+                            .ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        public static async Task<AnalysisResult>
+            GetAnalisysComputerVisio(System.IO.Stream stream)
+        {
+            VisionServiceClient client =
+                new VisionServiceClient("a051bcb1c4864f6188409e99010e177b");
+            VisualFeature[] features = {
+                                            VisualFeature.Adult,
+                                             VisualFeature.Faces,
+                                              VisualFeature.Color,
+                                               VisualFeature.Description,
+                                                VisualFeature.Tags,
+                                                 VisualFeature.ImageType,
+                                            VisualFeature.Categories };
+
+            return await client.AnalyzeImageAsync(stream, features); 
+        }
+
+        //xam.Plugin.Media
+        public static async Task<MediaFile> TakePhoto(bool useCamera) {
+            await Plugin.Media.CrossMedia.Current.Initialize();
+            if (useCamera)
+                if (!CrossMedia.Current.IsCameraAvailable ||
+                    !CrossMedia.Current.IsTakePhotoSupported)
+                    return null;
+
+            string nameSecuenceFile = new Random(DateTime.Now.Millisecond).Next(100, 999).ToString();
+            var file = useCamera ? await CrossMedia.Current.TakePhotoAsync(
+                    new StoreCameraMediaOptions
+                    {
+                        Directory = "FolderImagesCognitive"
+                    ,
+                        Name = $"TestImages-{nameSecuenceFile}.jpg"
+                    })
+                    : await CrossMedia.Current.PickPhotoAsync();
+
+            return file; 
+        }
+
+    }
+}
---
+++ XamarinCognitiveServices/XamarinCognitiveServices/XamarinCognitiveServices/ComputerVisioPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinCognitiveServices
+{
+	public partial class ComputerVisioPage : ContentPage
+	{
+		public ComputerVisioPage ()
+		{
+			InitializeComponent ();
+		}
+
+        Stream streamCopy; 
+        async void btnPhoto_Clicked(object sender, EventArgs e)
+        {
+            var useCamera = ((Button)sender).Text.Contains("cámara");
+
+            var file = await APIServices.TakePhoto(useCamera);
+            imgPhoto.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                streamCopy = new MemoryStream();
+                stream.CopyTo(streamCopy);
+                stream.Seek(0, SeekOrigin.Begin);
+                file.Dispose();
+                return stream;
+            });
+
+        }
+
+        async void btnAnalizeImage_Clicked(object sender, EventArgs e)
+        {
+            if (streamCopy != null)
+            {
+                streamCopy.Seek(0, SeekOrigin.Begin);
+                var vision = await APIServices.GetAnalisysComputerVisio(streamCopy);
+
+                var adulto = vision.Adult;
+                lblAdult.Text = String.Format("Contenido Adulto: {0} ({1})", adulto.IsAdultContent, adulto.AdultScore.ToString("P4"));
+                lblRacist.Text = String.Format("Contenido Racista: {0} ({1})", adulto.IsRacyContent, adulto.RacyScore.ToString("P4"));
+
+                var categorias = vision.Categories;
+                lblCategories.Text = "Categorias: ";
+                categorias.ToList().ForEach(cat => lblCategories.Text =
+                        lblCategories.Text + String.Format("{0} ({1}), ", cat.Name, cat.Score.ToString("P4")));
+
+                var color = vision.Color;
+                lblColor.Text = String.Format("Accent Color: {0}\nColor dominante:\nFondo: {1}\tFrente: {2}\n¿Es Blanco y Negro? {3}\nColores dominantes: ",
+                    color.AccentColor, color.DominantColorBackground,
+                    color.DominantColorForeground, color.IsBWImg);
+                color.DominantColors.ToList().ForEach(x => lblColor.Text = lblColor.Text + x + ", ");
+
+                var descripcion = vision.Description;
+                lblTags.Text = "Tags: ";
+                lblCaptions.Text = "Captions: ";
+                vision.Description.Tags.ToList().ForEach(tag => lblTags.Text = lblTags.Text + tag + ", ");
+                vision.Description.Captions.ToList().ForEach(cap => lblCaptions.Text = lblCaptions.Text + String.Format("{0} ({1}), ", cap.Text, cap.Confidence.ToString("P4")));
+
+                var caras = vision.Faces;
+                lblFaces.Text = "Caras: ";
+                caras.ToList().ForEach(cara => lblFaces.Text = lblFaces.Text + String.Format("{0} ({1}), ", cara.Gender, cara.Age));
+
+                var tags = vision.Tags;
+                lblTags2.Text = "Tags 2: ";
+                tags.ToList().ForEach(tag => lblTags2.Text = lblTags2.Text + String.Format("{0} - {1} ({2}), ", tag.Name, tag.Hint, tag.Confidence.ToString("P4")));
+            }
+        }
+    }
+}
