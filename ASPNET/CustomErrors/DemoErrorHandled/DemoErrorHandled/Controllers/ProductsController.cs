b78d3508b279002309c1d1f25f2b0a1411283cf1
Sergio Vargas MCT <serandvaraco@outlook.com>
2017-05-09 22:59:05 -0500

    Add Demo Exception Handling by unespacioparanet.com

---
+++ ASPNET/CustomErrors/DemoErrorHandled/DemoErrorHandled/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoErrorHandled.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        [HandleError(ExceptionType = typeof(NotImplementedException), View = "NotImplement")]
+        public ActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult NotImplement() {
+
+            return View(); 
+        }
+    }
+}
---
+++ ASPNET/CustomErrors/DemoErrorHandled/DemoErrorHandled/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoErrorHandled.MvcApplication" Language="C#" %>
