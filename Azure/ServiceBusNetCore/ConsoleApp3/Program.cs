1b37271ef87184fd51131c81aceb0d598bdebfad
Sergio Vargas Acosta <serandvaraco@outlook.com>
2018-12-03 19:50:30 -0500

    ServiceBus Net Core

---
+++ Azure/ServiceBusNetCore/ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace ServiceBusServer
+{
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus;
+    using Newtonsoft.Json;
+
+    internal class Program
+    {
+
+        private static void Main(String[] args) => MainAsync().GetAwaiter().GetResult();
+        private static async Task MainAsync()
+        {
+            IQueueClient queue = new QueueClient("Endpoint=sb://xxxxxxxx.servicebus.windows.net/;SharedAccessKeyName=send;SharedAccessKey=xxxyyyzzz=", "queue");
+
+            String[] countries = new[] { "Colombia", "Brazil", "Mexico", "Argentina", "Alemania", "Panama", "Chile", "Costa Rica", "España", "China", "Japon" };
+            Random rnd = new Random(DateTime.Now.Millisecond);
+
+            while (true)
+            {
+                String objectSerializer = JsonConvert.SerializeObject(new { message = $"Hello {countries[rnd.Next(10)]}" });
+                Byte[] objectBytes = Encoding.UTF8.GetBytes(objectSerializer);
+
+                await queue.SendAsync(new Message
+                {
+                    Body = objectBytes,
+                    ContentType = Encoding.UTF8.EncodingName,
+                    PartitionKey = Guid.NewGuid().ToString()
+                });
+
+                await Task.Delay(500);
+
+            }
+
+        }
+    }
+}
+
+
---
+++ Azure/ServiceBusNetCore/ServiceBusClient/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+namespace ServiceBusClient
+{
+    using System;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus;
+
+    internal class Program
+    {
+        private const String ServiceBusConnectionString = "Endpoint=sb://xxxxx.servicebus.windows.net/;SharedAccessKeyName=read;SharedAccessKey=xxxyyyzzz";
+        private const String QueueName = "queue";
+        private static IQueueClient queueClient;
+
+        private static void Main(String[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("TODAS LAS COLAS.");
+            Console.WriteLine("======================================================");
+
+            ReceivedMessages();
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        private static void ReceivedMessages()
+        {
+            MessageHandlerOptions messageHandlerOptions = new MessageHandlerOptions(ExceptionReceived)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        private static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Mensaje: Secuencia:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceived(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Exception {exceptionReceivedEventArgs.Exception}.");
+            ExceptionReceivedContext context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Detalle:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Path: {context.EntityPath}");
+            Console.WriteLine($"- Acción: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
